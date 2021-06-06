89636349bb88cc83754661194a709cd528c650bb
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-06 04:19:47 -0400

    Add settings toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import classNames from "classnames";
 import Timer from "./components/Timer";
 import "./App.scss";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setTimer, setTheme } from "./actions";
 
 const App = () => {
+  const [state, setState] = useState({
+    showSettings: false
+  });
+  const toggleSettings = () => {
+    setState({ ...state, showSettings: !state.showSettings });
+  };
   const theme = useSelector(state => state.theme) || "blue";
   const dispatch = useDispatch();
   const appStyles = classNames({
@@ -15,8 +22,12 @@ const App = () => {
     <main className={appStyles}>
       <h2>Pomomilk</h2>
       <Timer />
-      <h3 onClick={() => dispatch(setTimer(1500))}>settings</h3>
-      <h3 onClick={() => dispatch(setTheme("next"))}>theme: {theme}</h3>
+      <h3 onClick={() => toggleSettings()}>settings</h3>
+      {state.showSettings &&
+        <div className="settings">
+          <h3 onClick={() => dispatch(setTheme("next"))}>theme: {theme}</h3>
+        </div>
+      }
     </main>
   );
 };
