8fb40c9433b437e1b6b3360c8546a977cc30b2e9
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-07 00:10:07 -0400

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,11 @@ const About = (props) => {
   };
   return (
     <div className="About">
-      <h2>About</h2>
-      <h3>Made by Maria Regina Sirilan</h3>
+      <h2>Pomomilk</h2>
+      <h3>A simple interactive pomodoro timer</h3>
+      <h4>by Maria Regina Sirilan</h4>
+      <p>The <span>Pomodoro Technique</span> is a time management method that involves breaking down work into intervals called &quot;pomodoros&quot; (typically 25 min). These are separated by short breaks (3-5 min), with every fourth being a longer break (15-30 min).</p>
+      <h4 className="settings-toggle">See project on GitHub</h4>
       <h3 className="settings-toggle close" onClick={() => props.onClose(false)}>close</h3>
     </div>
   );
